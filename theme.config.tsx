b68160915e1bc9c51a3f8640b023d24af445c0a0
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2023-03-12 12:20:09 +0100

    feat images

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,9 +2,30 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Star Wars Databank</span>,
+  logo: (
+    <>
+      <img
+        src="/images/jedi_symbol.svg"
+        alt="Jedi symbol"
+        width="24"
+        height="24"
+      />
+      <span style={{ marginLeft: ".5em", fontWeight: 800 }}>
+        Star Wars Databank
+      </span>
+    </>
+  ),
   project: {
     link: "https://github.com/lethamburn/starwars-databank-v2",
+  },
+  primaryHue: 39,
+  banner: {
+    key: "2.0-release",
+    text: (
+      <a href="https://starwars-databank.vercel.app/about">
+        🎉 Star Wars Databank 2.0 is released. Read more →
+      </a>
+    ),
   },
 };
 
